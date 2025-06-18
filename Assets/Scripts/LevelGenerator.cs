09611b114a2b29d4dc2c2473174d7906bc1a28fc
farisimba <farisimbaa@gmail.com>
2025-06-19 02:47:30 +0800

    added gameover scene, game over when fall through camera

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,24 +1,28 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class LevelGenerator : MonoBehaviour
 {
     public GameObject platformPrefab;
+    public SpriteRenderer background;
 
     public int numberOfPlatforms;
-    public float levelWidth = 3f;
-    public float minY = .2f;
-    public float maxY = 1.5f;
+    public float minY;
+    public float maxY;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Vector3 spawnPosition = new Vector3();
+        Vector3 spawnPosition = new Vector3(0,0,0);
+        float minX = background.bounds.min.x;
+        float maxX = background.bounds.max.x;
 
         for (int i = 0; i < numberOfPlatforms; i++)
         {
             spawnPosition.y += Random.Range(minY, maxY);
-            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+            spawnPosition.x = Random.Range(minX, maxX);
 
+            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
         }
     }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,7 @@
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
@@ -6,6 +9,8 @@ public class Player : MonoBehaviour
     public float moveSpeed = 10f;
     float movement = 0f;
     Rigidbody2D rb;
+    public SpriteRenderer background;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,6 +21,12 @@ public class Player : MonoBehaviour
     void Update()
     {
         movement = Input.GetAxis("Horizontal") * moveSpeed;
+
+        Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
+        if (viewPos.y < 0)
+        {
+            GameOver();
+        }
     }
 
     void FixedUpdate()
@@ -24,4 +35,38 @@ public class Player : MonoBehaviour
         velocity.x = movement;
         rb.linearVelocity = velocity;
     }
+
+    void LateUpdate()
+    {
+        WrapAroundByBackground();
+    }
+
+    void WrapAroundByBackground()
+    {
+        float minX = background.bounds.min.x;
+        float maxX = background.bounds.max.x;
+        Vector3 pos = transform.position;
+
+        if (pos.x > maxX)
+        {
+            pos.x = minX;
+        }
+        else if (pos.x < minX)
+        {
+            pos.x = maxX;
+        }
+
+        transform.position = pos;
+    }
+
+    IEnumerator GameOverDelay()
+{
+    yield return new WaitForSeconds(1f); // Wait 1 second
+    SceneManager.LoadScene("GameOver");
+}
+
+void GameOver()
+{
+    StartCoroutine(GameOverDelay());
+}
 }
