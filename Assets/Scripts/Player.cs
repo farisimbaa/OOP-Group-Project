31a7806a8be9e76c821d02d72b9dab8f996145b8
farisimba <farisimbaa@gmail.com>
2025-06-18 21:22:31 +0800

    added assets and basic physics

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    float movement = 0f;
+    Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal") * moveSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.linearVelocity;
+        velocity.x = movement;
+        rb.linearVelocity = velocity;
+    }
+}
