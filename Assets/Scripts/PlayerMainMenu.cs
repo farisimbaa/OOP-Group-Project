9d2f8ae2227851e1ec4577587bd5bf8f1f43c042
farisimba <farisimbaa@gmail.com>
2025-06-21 14:23:46 +0800

    added visual on main menu

---
+++ Assets/Scripts/PlayerMainMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerMainMenu : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
