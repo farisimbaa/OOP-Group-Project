5952e87342ee7b89ed593eee592df223c63fd7e5
romkerome <ahmad_24000251@utp.edu.my>
2025-08-02 03:25:10 +0800

    latest

--- Assets/Scripts/PlayerLevel2.cs
+++ Assets/Scripts/PlayerLevel2.cs
@@ -3,47 +3,40 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections;
 
+
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerLevel2 : MonoBehaviour
 {
-    [Header("Movement")]
     public float moveSpeed = 10f;
     private float movement;
     private Rigidbody2D rb;
 
-    [Header("Background")]
     public SpriteRenderer background;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
 
-    [Header("Hearts UI")]
     public Image[] heartIcons;
     public Sprite fullHeart;
     public Sprite emptyHeart;
 
-    [Header("Lives")]
-    public int lives = 3;
     private bool isInvincible = false;
     public float invincibilityTime = 1f;
+    public int lives = 3;
 
-    [Header("Character Sprites")]
     public Sprite[] characterSprites;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
 
-        // Set character sprite from PlayerPrefs
         int selectedCharacterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0);
-        if (selectedCharacterIndex < characterSprites.Length)
-        {
-            GetComponent<SpriteRenderer>().sprite = characterSprites[selectedCharacterIndex];
-        }
+        GetComponent<SpriteRenderer>().sprite = characterSprites[selectedCharacterIndex];
     }
 
     void Update()
     {
         movement = Input.GetAxis("Horizontal") * moveSpeed;
 
-        // Flip sprite based on direction
         if (movement > 0.1f)
             transform.localScale = new Vector3(1f, 1f, 1f);
         else if (movement < -0.1f)
@@ -70,12 +63,11 @@ public class PlayerLevel2 : MonoBehaviour
 
     void WrapAroundBackground()
     {
-        if (background == null) return;
-
         float minX = background.bounds.min.x;
         float maxX = background.bounds.max.x;
 
         Vector3 pos = transform.position;
+
         if (pos.x > maxX) pos.x = minX;
         else if (pos.x < minX) pos.x = maxX;
 
@@ -130,13 +122,7 @@ public class PlayerLevel2 : MonoBehaviour
 
     void GameOver()
     {
-        int final = 0;
-
-        if (ScoreSystem.Instance != null)
-        {
-            final = ScoreSystem.Instance.GetScore();
-        }
-
+        int final = ScoreSystem.Instance.GetScore();
         PlayerPrefs.SetInt("FinalScore", final);
         PlayerPrefs.Save();
         SceneManager.LoadScene("GameOver");
